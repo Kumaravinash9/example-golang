f47fe426742eb6055f2e4133a9b062860029e2ed
unknown <bhaiavinash9@gmail.com>
2023-01-29 02:38:41 +0530

    Config sql

--- config/db.go
+++ config/db.go
@@ -1,25 +1,22 @@
 package config
 
 import (
-	"fmt"
-	"os"
+	"database/sql"
 
-	"gorm.io/driver/postgres"
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Db() *gorm.DB {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s dbname=%s user=%s password=%s sslmode=disable TimeZone=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_TIMEZONE"),
-	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	url := "root" + ":" + "Bhai@vi9" + "@tcp(" + "localhost" + ":" + "3306" + ")/" + "dhandolat" + "?sql_mode=STRICT_ALL_TABLES&tls=skip-verify&multiStatements=true&parseTime=true&charset=utf8mb4,utf8"
+	client, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err.Error())
+	}
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: client,
+	}), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/mholt/binding v0.3.0 // indirect
 	github.com/ruyjfs/lab-bank-go v0.0.0-20201201022907-74dec24f28ee
 	github.com/vektah/gqlparser/v2 v2.1.0
+	gorm.io/driver/mysql v1.4.5 // indirect
 	gorm.io/driver/postgres v1.0.5
-	gorm.io/gorm v1.20.8
+	gorm.io/gorm v1.24.3
 )
